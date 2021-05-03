b689726882b01156d977541438b5ee776a7d3b78
Linnea Wikberg <lvikberg@kth.se>
2021-05-03 10:13:46 +0200

    Changed layout of currentMessageView

--- Software/webapp/src/views/currentMessageView.js
+++ Software/webapp/src/views/currentMessageView.js
@@ -4,26 +4,18 @@ const CurrentMessageView = ({ currentMessage }) => {
   return (
     <div>
 
-      <h1 className="header center orange-text">
-        Current Message
-      </h1>
-      <h3> {currentMessage} </h3>
-      <br />
-      <div className="row center">
-        <NavLink
-          to="/newmessage"
-          className="btn btn-large orange hoverable"
-        >
-          New message
-        </NavLink>
-        <NavLink
-          to="/"
-          className="btn btn-large orange hoverable"
-        >
-          Home
-        </NavLink>
+    <div class="row">
+        <div class="col l6 offset-l3 valign">
+        <h4>Current Message</h4>
+          <div class="card">
+            <div class="card-content">
+              <p>{currentMessage}</p>
+            </div>
+          </div>
+          <NavLink to="/newmessage" className="btn hoverable">New message<i class="material-icons left white-text">create</i></NavLink>
+        </div>
       </div>
-     
+
     </div>
   );
 };
